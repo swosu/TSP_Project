d91a596b5577c641bd3c41d7c2654aa8a744faf0
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-03-10 05:41:18 -0600

    able to build a random path with no repeats.

--- Jeremy/Guess_and_Check_Path_Finder.py
+++ Jeremy/Guess_and_Check_Path_Finder.py
@@ -5,14 +5,23 @@ class Guess_and_Check_Path_Finder:
         self.cities_to_visit = []
         self.file_name = ''
         self.distance_table = []
+        self.path = []
 
     def say_hello(self):
         #self.say_hello()
         print('So that worked...')
 
     def find_path(self):
+        import random
         self.get_cities_to_visit_list()
-
+        for index in range(0, self.city_count):
+            #print(f'our index is {index}.')
+            next_city = random.choice(self.cities_to_visit)
+            print(f'our next city is: {next_city}.')
+            self.path.append(next_city)
+            print(f'our current path is: {self.path}.')
+            self.cities_to_visit.remove(next_city)
+            print(f'remaining cities to visit: {self.cities_to_visit}.')
 
     def get_cities_to_visit_list(self):
         for index in range(0, self.city_count):
